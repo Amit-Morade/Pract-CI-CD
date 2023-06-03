27ec59f92b6085a337f6a4da9831d7728cda0c05
Amit Morade <amitmorade19@gmail.com>
2023-06-03 23:13:35 +0530

    core: made changes to App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App Component', () => {
+  it("should render hello world msg on screen", () => {
+    render(<App />);
+    screen.debug();
+  })
+})
